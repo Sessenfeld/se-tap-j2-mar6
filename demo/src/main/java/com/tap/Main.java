ebf6119d89a9d2eab13072e8d8f4d0c0025b27bd
Sam Essenfeld <67374884+Sessenfeld@users.noreply.github.com>
2025-03-06 16:28:40 +0000

    this is my commit at the end of the TAP training JAva 2 session

---
+++ demo/src/main/java/com/tap/Main.java
@@ -0,0 +1,11 @@
+package com.tap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        String[] ar = {"Sam", "TAP", "Zest"};
+        C_ar.main(args);
+        C_ar.arrayEg(ar);
+    }
+}
